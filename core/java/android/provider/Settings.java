0f2c5bd7097772f47622ab700896a370d46c8c02
cristianomatos <cristianobmatos@gmail.com>
2016-12-07 14:50:41 +0800

    Recents FAB Clear All Button Tweaks [1/2] Ported to Nougat By: -varund7726 and -xyyx
    This modification is a squash of Multiple peoples work seems credits got lost along the way..
    *Add clear all recents action to recents panel -0xD34D
    *Fix - place Clear Recents button above the nav bar -rajyengi
    *Fix up 'clear recents' UX. -maniac103
    *recents panel clear all button tweaks -cristianomatos
    *replace recents clear all button with a fab button -altaf-mahdi
    *add  bottom right/left options to fab location -altaf-mahdi
    *add top center and bottom center options to fab location -temasek

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3986,6 +3986,18 @@ public final class Settings {
         public static final String STATUS_BAR_DAYLIGHT_HEADER_PACK = "status_bar_daylight_header_pack";
 
         /**
+         * show clear all recents button
+         * @hide
+         */
+        public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
+
+        /**
+         * location of the clear all rectents button
+         * @hide
+         */
+        public static final String  RECENTS_CLEAR_ALL_LOCATION = "recents_clear_all_location";
+
+        /**
          * Current active provider - available currently "static" "daylight"
          * @hide
          */
